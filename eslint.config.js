50d006acc1f56e6c43cb56e9e255734a867d6154
tsx <miladtsx@gmail.com>
2025-12-30 11:54:58 +0330

    Refine ESLint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,35 @@ import tseslint from "typescript-eslint";
 import nextPlugin from "@next/eslint-plugin-next";
 import reactHooks from "eslint-plugin-react-hooks";
 
-export default tseslint.config(
+const [tsRecommended, tsTypeChecked] = tseslint.configs.recommendedTypeChecked;
+
+export default [
   { ignores: ["dist", ".next"] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      nextPlugin.configs.recommended,
-    ],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: { project: "./tsconfig.json" },
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: {
         ...globals.browser,
         React: true,
       },
     },
     plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tseslint.plugin,
       "react-hooks": reactHooks,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tsRecommended.rules,
+      ...tsTypeChecked.rules,
+      ...nextPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-);
+];
